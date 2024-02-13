acc58e851f4ea59881e241cd83a8ba3e31da23c5
ignaarguello <igna.arguello2110@hotmail.com>
2024-02-12 21:43:03 -0300

    Navbar Interior en proceso

--- src/components/TituloPages/TituloPages.jsx
+++ src/components/TituloPages/TituloPages.jsx
@@ -1,8 +1,8 @@
 export default function Example(props) {
     return (
         <div className="flex flex-col">
-            <h4 className="bg-black w-max text-white p-2">Sección</h4>
-            <h2 className=" text-3xl font-bold font-light text-lime-700">
+            {/* <h4 className="bg-black w-max text-white p-2">Sección</h4> */}
+            <h2 className="text-3xl font-bold tracking-tight text-lime-700">
                 {props.titulo}
             </h2>
         </div>
--- src/pages/Informes/Informes.jsx
+++ src/pages/Informes/Informes.jsx
@@ -1,9 +1,12 @@
 import TituloPages from '../../components/TituloPages/TituloPages'
 
+import NavbarInside from '../../components/NavbarInside/NavbarInside'
+
 export default function Informes() {
   return (
     <div>
-      <TituloPages titulo='Informes' />
+      {/* <TituloPages titulo='Informes' /> */}
+      <NavbarInside />
     </div>
   )
 }
