ac9f9f8bcb423fbc1b0fe53c497d9a0d759758b4
ignaarguello <igna.arguello2110@hotmail.com>
2024-03-14 01:35:27 -0300

    Add: mas mejoras de estilos en home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,8 +8,8 @@ export default function Home() {
         <div>
             <NavbarInside />
             <div id='container-image-title_Home'>
-                <h2 id='titulo-grow-weed' className='text-3xl font-extralight tracking-tight text-gray-900 animate__animated animate__fadeIn animate__delay-3s'>Grow Weed</h2>
-                <h3 id='titulo-making-nature' className='text-1xl font-extralight tracking-tight text-gray-600 animate__animated animate__fadeIn animate__delay-3s'>Making Nature</h3>
+                <h2 id='titulo-grow-weed' className='text-3xl font-extralight tracking-tight text-gray-900 animate__animated animate__fadeIn animate__delay-2s'>Grow Weed</h2>
+                <h3 id='titulo-making-nature' className='text-1xl font-extralight tracking-tight text-gray-500 animate__animated animate__fadeIn animate__delay-2s'>Making Nature</h3>
             </div>
         </div>
     )
