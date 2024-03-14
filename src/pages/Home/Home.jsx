91ffc92b3ac2dd35c0ed614df0c35eb8c1600e87
ignaarguello <igna.arguello2110@hotmail.com>
2024-03-14 01:33:11 -0300

    Add: animate.css + modificaciones en home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import './Home.css'
 import NavbarInside from '../../components/NavbarInside/NavbarInside'
+import 'animate.css';
 
 
 export default function Home() {
@@ -7,8 +8,8 @@ export default function Home() {
         <div>
             <NavbarInside />
             <div id='container-image-title_Home'>
-                <h2 id='titulo-grow-weed' className='text-3xl font-extralight tracking-tight text-gray-900'>Grow Weed</h2>
-                <h3 className='text-1xl font-extralight tracking-tight text-gray-600'>Making Nature</h3>
+                <h2 id='titulo-grow-weed' className='text-3xl font-extralight tracking-tight text-gray-900 animate__animated animate__fadeIn animate__delay-3s'>Grow Weed</h2>
+                <h3 id='titulo-making-nature' className='text-1xl font-extralight tracking-tight text-gray-600 animate__animated animate__fadeIn animate__delay-3s'>Making Nature</h3>
             </div>
         </div>
     )
