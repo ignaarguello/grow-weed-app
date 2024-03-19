27750ab478310a1384975d0c9a29ba088bf92e1a
ignaarguello <igna.arguello2110@hotmail.com>
2024-03-19 00:04:01 -0300

    Checking

--- src/components/ComponentesInformes/CInformes_1/CInformes_1.jsx
+++ src/components/ComponentesInformes/CInformes_1/CInformes_1.jsx
@@ -42,6 +42,7 @@ export default function CInformes_1() {
                         </li>
                     ))}
                 </ul>
+                Hola
             </div>
         </div>
     )
