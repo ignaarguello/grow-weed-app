5558be5d169bd408695363b65a91cb9cc8b51cb2
ignaarguello <igna.arguello2110@hotmail.com>
2024-09-27 17:19:18 -0300

    Change: BaseURL variable

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,3 +1,3 @@
-/* export const URL_BASE = 'https://gwa-backend-2-production.up.railway.app'; */
+export const URL_BASE = 'https://gwa-backend-2-production.up.railway.app';
 
-export const URL_BASE = 'http://localhost:3000'; 
+/* export const URL_BASE = 'http://localhost:3000';  */
