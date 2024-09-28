163e89b89b83956ba875cfaa808f58d2e3798a36
ignaarguello <igna.arguello2110@hotmail.com>
2024-09-27 21:26:54 -0300

    Add: mejoras en ux de informes

--- src/components/ComponentesInformes/CInformes_1/CInformes_1.jsx
+++ src/components/ComponentesInformes/CInformes_1/CInformes_1.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { informesActions } from '../../../redux/actions/Informes';
 import { Link } from 'react-router-dom';
+import { TiDelete } from "react-icons/ti";
 
 export default function CInformes_1() {
     const dispatch = useDispatch()
@@ -14,27 +15,27 @@ export default function CInformes_1() {
         dispatch(getInformes())
         console.log(informes)
     }, []);
-    
+
     return (
         <div id='Contenedor-CI_1'>
             <h2 id='titulo-seccionInformes' className='font-extralight tracking-tight text-white'>Sección Informes</h2>
             <div id='contenedor-dataInformes'>
                 <ul role="list" className="divide-y divide-gray-100">
                     {informes?.map((item) => (
-                        <li key={item.nombre} className="flex justify-between gap-x-6 p-2 my-4">
+                        <li key={item.nombre} className="flex justify-between gap-x-2 p-2 my-4">
                             <div className="flex min-w-0">
                                 <img className="h-14 w-14 flex-none rounded-full bg-gray-50 border-solid border-2 border-amber-700" style={{ objectFit: 'cover' }} src={'https://marketplace.canva.com/_zKoo/MAFVqF_zKoo/1/s2/canva-sin-t%C3%ADtulo-MAFVqF_zKoo.jpg'} alt="" />
                                 <div className="min-w-0 flex-auto ml-2 mt-1">
-                                    <p className="text-sm font-bold leading-6 text-gray-900">{item.nombre.toUpperCase()}</p>
+                                    <p id='TituloNombreItem_CInformes1' className="text-sm font-bold leading-6 text-gray-900">{item.nombre.toUpperCase()}</p>
                                     <p className="truncate text-xs leading-5 text-lime-900 font-medium">{item.fecha}</p>
                                 </div>
                             </div>
-                            <Link to={`${item._id}`}
-                                className="rounded-md bg-black flex items-center px-3 py-2 text-sm font-light text-white shadow-sm hover:bg-lime-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                
-                            >
+                            <Link to={`${item._id}`} id="ButtonVerMas_CInformes1">
                                 Ver Más +
                             </Link>
+                            <div id='ContenedorIconoDelete_CInformes1' >
+                                Eliminar
+                            </div>
                         </li>
                     ))}
                 </ul>
