56fb38b79e065d8b482b3a65ced41430f826d78b
ignaarguello <igna.arguello2110@hotmail.com>
2024-10-07 18:58:54 -0300

    Add: panel en CDesarrollo_2

---
+++ src/components/CDesarrollo/CDesarrollo_2/CDesarrollo_2.jsx
@@ -0,0 +1,20 @@
+import './CDesarrollo_2.css'
+
+export default function CDesarrollo_2() {
+    return (
+        <div id='ContenedorGeneral_CDesarrollo_2'>
+            <section id='SectionIzquierdo_CDesarrollo_2'>
+                <div className="SubContenedorSection_CDesarrollo_2">
+                    <h2>+</h2>
+                </div>
+                <div className="SubContenedorSection_CDesarrollo_2"></div>
+            </section>
+            <aside id='AsideDerecho_CDesarrollo_2'>
+                <div className="SubContenedorAside_CDesarrollo_2">
+                    <span>0</span>
+                    <span>Plantas Totales:</span>
+                </div>
+            </aside>
+        </div>
+    )
+}
--- src/pages/Desarrollo/Desarrollo.jsx
+++ src/pages/Desarrollo/Desarrollo.jsx
@@ -1,12 +1,14 @@
 import './Desarrollo.css'
 
 //Componentes
-import CDesarrollo_1 from '../../components/CDesarrollo/CDesarrollo_1'
+import CDesarrollo_1 from '../../components/CDesarrollo/CDesarrollo_1/CDesarrollo_1'
+import CDesarrollo_2 from '../../components/CDesarrollo/CDesarrollo_2/CDesarrollo_2'
 
 export default function Desarrollo() {
   return (
     <div id='ContenedorPageDesarrollo_Desarrollo'>
       <CDesarrollo_1 />
+      <CDesarrollo_2 />
     </div>
   )
 }
