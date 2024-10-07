665c753ad39271641c48a1679d9bf16814e9b1fd
ignaarguello <igna.arguello2110@hotmail.com>
2024-10-07 20:18:23 -0300

    Update: valores en CDesarrollo2

--- src/components/CDesarrollo/CDesarrollo_2/CDesarrollo_2.jsx
+++ src/components/CDesarrollo/CDesarrollo_2/CDesarrollo_2.jsx
@@ -4,18 +4,17 @@ export default function CDesarrollo_2() {
     return (
         <div id='ContenedorGeneral_CDesarrollo_2'>
             <div className="Indicadores_CDesarrollo_2">
-                <span className='ItemContador_CDesarrollo_2'>3</span>
-                <span className='ItemTitulo_CDesarrollo_2'>Totales</span>
-            </div>
-            <div className="Indicadores_CDesarrollo_2">
-                <span className='ItemContador_CDesarrollo_2'>1</span>
+                <span className='ItemContador_CDesarrollo_2'>2</span>
                 <span className='ItemTitulo_CDesarrollo_2'>Crecimiento</span>
             </div>
             <div className="Indicadores_CDesarrollo_2">
-                <span className='ItemContador_CDesarrollo_2'>2</span>
+                <span className='ItemContador_CDesarrollo_2'>1</span>
                 <span className='ItemTitulo_CDesarrollo_2'>Cosechadas</span>
             </div>
-
+            <div className="Indicadores_CDesarrollo_2">
+                <span className='ItemContador_CDesarrollo_2'>3</span>
+                <span className='ItemTitulo_CDesarrollo_2'>Totales</span>
+            </div>
         </div>
     )
 }
